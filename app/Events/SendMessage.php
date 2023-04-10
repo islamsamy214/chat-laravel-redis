f528ae2d0c4da656960c17c5452343bb2dae9722
islamsamy214 <islamsamy214@gmail.com>
2023-04-10 14:50:44 +0200

    initial commit

---
+++ app/Events/SendMessage.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+
+class SendMessage implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $data = [];
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($data)
+    {
+        array_push($this->data, $data);
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('user-channel')
+        ];
+        // return [
+        //     new PrivateChannel('channel-name'),
+        // ];
+    }
+
+    public function broadcastAs()
+    {
+        return 'UserEvent';
+    }
+
+    public function broadcastWith()
+    {
+        return ['data' => $this->data];
+    }
+}
